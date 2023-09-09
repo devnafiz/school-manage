f2216b636eb15c44bbb1d323fbe7ba6baa28035a
nafiz <nafiz.2011@yahoo.com>
2023-09-09 15:24:16 +0600

    add hostel

--- app/Http/Controllers/Backend/HostelContoller.php
+++ app/Http/Controllers/Backend/HostelContoller.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Hostel;
+use App\Models\HostelRoom;
+use App\Models\RoomType;
 
 class HostelContoller extends Controller
 {
